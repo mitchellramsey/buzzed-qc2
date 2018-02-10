0037e0cf3e9bf9aa5961d0c6c78b5335d5c455e5
Kurtis <kcarpe28@gmail.com>
2018-02-10 11:23:24 -0500

    go dude

--- assets/javascript/modals.js
+++ assets/javascript/modals.js
@@ -3,30 +3,4 @@
         $('#myModal').modal('show');
     });
 
-
-    //the below modal is in the index.html 
-    // <!-- modal -->
-    // <div class="modal fade" id="myModal" tabindex="-1" role="dialog">
-    //     <div class="modal-dialog" role="document">
-    //         <div class="modal-content">
-    //             <div class="modal-header" id="modalHeadingLook">
-    //                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-    //                     <span aria-hidden="true">&times;</span>
-    //                 </button>
-    //                 <h2 class="modal-title">Beer Review</h2>
-    //             </div>
-    //             <div class="modal-body" id="modalBodyLook">
-    //                 <p>I rate this beer a
-    //                     <span>
-    //                         <input type="text" id="user-review">
-    //                     </span>
-    //                     <span> / 5</span>
-    //                     <p>
-    //             </div>
-    //             <div class="modal-footer" id="modalFooterLook">
-    //                 <button type="button" class="btn btn-primary">Submit Beer Review</button>
-    //                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>
+    $("#")
